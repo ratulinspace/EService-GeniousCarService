a512c4a12f566ea087d030ec40f0cceae3771338
ratulinspace <i.shahriarislam.ratul@gmail.com>
2022-04-23 13:08:11 +0600

    Carousel added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import Experts from '../Experts/Experts';
 import Services from '../Services/Services';
+import "./Home.css";
 
 const Home = () => {
     return (
-        <div>
+        <>
+            <Banner></Banner>
             <Services></Services>
-        </div>
+            <Experts></Experts>
+        </>
     );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -14,7 +14,7 @@ const Services = () => {
 
     return (
         <div>
-            <h1 className='services-title'>Our services</h1>
+            <h1 className='services-title mt-5'>Our services</h1>
             <div className='services-container'>
                 {
                     services.map(service => <Service
